4eaff24b99aaa355b60ef76d78468d5b181518cb
JayeshPC <Jayeshsutar1108@gmail.com>
2023-11-07 12:40:56 +0530

    Personal PorFolio initial state commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('typewriterAnimation', [
+      transition(':enter', [
+        style({ width: '0' }),
+        animate('{{ speed }}', style({ width: '*' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'TailApplication';
-  imagePath: string = './assets/Images/groot.png'; 
+  imagePath: string = './assets/Images/Profile-bg.png'; 
   starlordpath: string = './assets/Images/starLord.png';
   Rocketpath: string = './assets/Images/Rocket.png'; 
   Gamorapath: string = './assets/Images/Gamora.png'; 
